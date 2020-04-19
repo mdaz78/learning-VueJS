e4141bd6fd4c938dd33a7d60cc7f497ed71ab9f0
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-04-19 12:12:15 +0530

    Solve Exercise 2

--- Exercises/Exercise 2/app.js
+++ Exercises/Exercise 2/app.js
@@ -1,6 +1,14 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
-        }
-    });
+  el: '#exercise',
+  data: {
+    value: ''
+  },
+  methods: {
+    alertMe: function () {
+      alert("Alert!");
+    },
+    updateValue: function (event) {
+      this.value = event.target.value
+    }
+  }
+});
