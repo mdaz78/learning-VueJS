c5a60785bcdf602360d985b9265cc989141f5ea4
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-04-20 15:17:32 +0530

    Solve Exercise 4

--- Exercises/Exercise 4/app.js
+++ Exercises/Exercise 4/app.js
@@ -1,11 +1,44 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    effectClass: {
+      highlight: true,
+      shrink: false
+    },
+    userClass: '',
+    isVisible: true,
+    myStyle: {
+      width: '200px',
+      height: '500px',
+      backgroundColor: 'gray'
+    },
+    progressBar: {
+      width: '0px',
+      height: '20px',
+      backgroundColor: 'red'
+    }
   },
   methods: {
-    startEffect: function() {
-    
+    startEffect: function () {
+      const vm = this;
+      setInterval(function () {
+        vm.effectClass.highlight = !vm.effectClass.highlight;
+        vm.effectClass.shrink = !vm.effectClass.shrink;
+      }, 1000)
+    },
+    startProgress: function() {
+      const vm = this;
+      let width = 10;
+      const move = function() {
+        width = width + 10;
+        console.log("running")
+        if (width < 500) {
+          vm.progressBar.width = width + "px";
+        } else {
+          clearInterval(interval)
+        }
+      }
+      const interval = setInterval(move,500)
     }
   }
 });
