2a7023a802438517dea2301c3d410f5cf24a79c8
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-04 01:48:22 +0530

    Implement the giveUp method

--- Projects/the-monster-slayer/app.js
+++ Projects/the-monster-slayer/app.js
@@ -34,7 +34,7 @@ new Vue({
       }
     },
     giveUp: function () {
-      console.log("giveUp")
+      this.gameIsRunning = false;
     },
     calculateDamage: function (min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min)
