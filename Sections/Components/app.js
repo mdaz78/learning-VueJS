bea79935243b9b0e00c27eb40ad939d1ad0d9f71
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-04 14:25:25 +0530

    Learn to Create a Component

---
+++ Sections/Components/app.js
@@ -0,0 +1,17 @@
+Vue.component("my-cmp", {
+  data: function() {
+    return {
+      status: "Critical"
+    };
+  },
+  template: `<p>Server Status {{ status }} <button @click="changeStatus">Change</button></p>`,
+  methods: {
+    changeStatus: function() {
+      this.status = "Normal";
+    }
+  }
+});
+
+new Vue({
+  el: "#app"
+})
