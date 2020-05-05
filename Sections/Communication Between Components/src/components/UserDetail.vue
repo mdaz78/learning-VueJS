949ccad83a919898519c0bc9524951bc6f29521f
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-05 15:00:05 +0530

    Learn how to have default values and create function in child components

--- Sections/Communication Between Components/src/components/UserDetail.vue
+++ Sections/Communication Between Components/src/components/UserDetail.vue
@@ -1,19 +1,33 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-        <p>User Name: {{name}}</p>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+  </div>
 </template>
 
 <script>
-    export default {
-      props: ['name']
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "Max",
+      required: true // You don't use default if you are using required
     }
+  },
+  methods: {
+    switchName() {
+      return this.name
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+div {
+  background-color: lightcoral;
+}
 </style>
