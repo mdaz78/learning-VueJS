f139f51fb448b1d33b5333370a2f9a48dc3d258c
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-05 17:01:14 +0530

    Learn to use <slot></slot>

--- Sections/Advanced Component Usage/src/App.vue
+++ Sections/Advanced Component Usage/src/App.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <myQuote>
+          <h2>Quote</h2>
+          <p>A Wonderful Quote!</p>
+        </myQuote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Quote from "./components/Quote.vue";
 
-    export default {
-        
-    }
+export default {
+  data: function() {
+    return {
+      quote: "A Wonderful Quote!"
+    };
+  },
+  components: {
+    myQuote: Quote
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
