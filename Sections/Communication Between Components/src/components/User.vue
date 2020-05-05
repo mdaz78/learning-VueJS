433f2246323e68eab9c1aa745c3c5af43b1ab855
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-05 15:14:06 +0530

    Learn communication with callback functions

--- Sections/Communication Between Components/src/components/User.vue
+++ Sections/Communication Between Components/src/components/User.vue
@@ -1,47 +1,52 @@
 <template>
-  <div class="component">
-    <h1>The User Component</h1>
-    <p>I'm an awesome User!</p>
-    <button @click="changeName">Change my name</button>
-    <hr />
-    <div class="row">
-      <div class="col-xs-12 col-sm-6">
-        <app-user-detail
-          :name="name"
-          @resetName="name = $event"
-        ></app-user-detail>
-      </div>
-      <div class="col-xs-12 col-sm-6">
-        <app-user-edit></app-user-edit>
-      </div>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <p>Name: {{ name }}</p>
+        <button @click="changeName">Change my name</button>
+        <hr/>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail
+                        :name="name"
+                        @resetName="name = $event"
+                        :resetFunction="resetFunction"
+                ></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import UserDetail from "./UserDetail.vue";
-import UserEdit from "./UserEdit.vue";
+  import UserDetail from "./UserDetail.vue";
+  import UserEdit from "./UserEdit.vue";
 
-export default {
-  data: function() {
-    return {
-      name: "Max"
-    };
-  },
-  methods: {
-    changeName() {
-      this.name = "Anna";
+  export default {
+    data: function () {
+      return {
+        name: "Max"
+      };
+    },
+    methods: {
+      changeName() {
+        this.name = "Anna";
+      },
+      resetFunction() {
+        this.name = "Zafar";
+      }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
     }
-  },
-  components: {
-    appUserDetail: UserDetail,
-    appUserEdit: UserEdit
-  }
-};
+  };
 </script>
 
 <style scoped>
-div {
-  background-color: lightblue;
-}
+    div {
+        background-color: lightblue;
+    }
 </style>
--- Sections/Communication Between Components/src/components/UserDetail.vue
+++ Sections/Communication Between Components/src/components/UserDetail.vue
@@ -1,37 +1,42 @@
 <template>
-  <div class="component">
-    <h3>You may view the User Details here</h3>
-    <p>Many Details</p>
-    <p>User Name: {{ switchName() }}</p>
-    <button @click="resetName">Reset Name</button>
-  </div>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <!--    <button @click="resetName">Reset Name</button>-->
+        <button @click="resetFunction()">Reset Name</button>
+    </div>
 </template>
 
 <script>
-export default {
-  props: {
-    name: {
-      type: String,
-      required: true // You don't use default if you are using required
-    }
-  },
-  methods: {
-    switchName() {
-      return this.name
-        .split("")
-        .reverse()
-        .join("");
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true // You don't use default if you are using required
+      },
+      resetFunction: {
+        type: Function,
+        required: true
+      }
     },
-    resetName() {
-      let name = 'zafar'
-      this.$emit("resetName", name)
+    methods: {
+      switchName() {
+        return this.name
+          .split("")
+          .reverse()
+          .join("");
+      },
+      resetName() {
+        let name = "zafar";
+        this.$emit("resetName", name);
+      }
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
-div {
-  background-color: lightcoral;
-}
+    div {
+        background-color: lightcoral;
+    }
 </style>
