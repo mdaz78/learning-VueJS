c00b52e2b68cc1cc25f06d5c0aa00283aa61f045
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-05 16:14:58 +0530

    Add Advanced Component Usage Project Directory

--- Sections/Communication Between Components/src/components/UserEdit.vue
+++ Sections/Communication Between Components/src/components/UserEdit.vue
@@ -15,7 +15,7 @@ export default {
   methods: {
     editAge() {
       this.userAge = 30;
-      eventBus.$emit("ageWasEdited", this.userAge);
+      eventBus.changeAge(this.userAge)
     }
   }
 };
