7acf8cb651b1ff165d664a93ee5db6104b443cf2
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-06 17:59:34 +0530

    Setup project for Animations and Transitions

---
+++ Sections/Animations And Transitions/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped></style>
