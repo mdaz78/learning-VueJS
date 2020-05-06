aca5eb38a6d835a478b57e5f283715cd60031136
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-06 15:22:47 +0530

    Learn to Add Binding Value using directives

--- Sections/Using and Creating Directives/src/App.vue
+++ Sections/Using and Creating Directives/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'<strong>Strong text</strong>'"></p>
+      </div>
     </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <p v-highlight="'red'">Color this</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- Sections/Using and Creating Directives/src/main.js
+++ Sections/Using and Creating Directives/src/main.js
@@ -1,7 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    console.log("called");
+    console.log(el);
+    console.log(binding);
+    console.log(vnode);
+    el.style.backgroundColor = binding.value;
+  }
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
