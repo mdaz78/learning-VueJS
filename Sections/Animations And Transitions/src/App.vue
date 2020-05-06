06f465b57731e14f4aa83aeceabf56f75f7e5e3f
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-06 18:09:39 +0530

    Learn to use transition property with single element

--- Sections/Animations And Transitions/src/App.vue
+++ Sections/Animations And Transitions/src/App.vue
@@ -2,14 +2,41 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Animations</h1>
+        <h1 class="my-3">Animations</h1>
+        <button class="btn btn-primary" @click="show = !show">
+          Show Alert
+        </button>
+        <transition name="fade">
+          <div class="alert alert-info mt-5" v-if="show">This is some info</div>
+        </transition>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      show: false
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+
+.fade-leave {
+}
+
+.fade-leave-active {
+  transition: opacity 0.5s;
+  opacity: 0;
+}
+</style>
