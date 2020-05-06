0193dd72b92650accec24a2b4e2edf1041f23154
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-05-06 16:15:27 +0530

    Learn to add complex values to directives

--- Sections/Using and Creating Directives/src/App.vue
+++ Sections/Using and Creating Directives/src/App.vue
@@ -12,13 +12,50 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Directives</h1>
         <p v-highlight:background.delay="'yellow'">Color this</p>
+        <p v-local-highlight:background.delay.blink="{mainColor: 'red', secondColor: 'green', delay: 50}">Color this</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers["delay"]) {
+          delay = binding.value.delay;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style></style>
